17bb1c059ac33cca29a3722dbc25db0969bffa91
AdminFedora <segor6052@gmail.com>
2025-01-30 18:19:23 +0200

    1.1

--- src/Background/Background.cpp
+++ src/Background/Background.cpp
@@ -43,14 +43,14 @@ void Background::Initialize(){
 }
 
 std::vector<Color> Background::GetCellColors() {
-    Color green = {34, 139, 34, 255}; //0
-    Color red = {220, 20, 60, 255};   //1
-    Color orange = {255, 140, 0, 255};//2
-    Color purple = {128, 0, 128, 255};//3
-    Color cyan = {0, 255, 255, 255};  //4
-    Color blue = {0, 0, 255, 255};    //5
-    Color darkGray = {26, 31, 40, 255};//6
-    Color yellow = {255, 255, 0, 255};//7
+    Color green = {34, 139, 34, 255};   //0
+    Color red = {220, 20, 60, 255};     //1
+    Color orange = {255, 140, 0, 255};  //2
+    Color purple = {128, 0, 128, 255};  //3
+    Color cyan = {0, 255, 255, 255};    //4
+    Color blue = {0, 0, 255, 255};      //5
+    Color darkGray = {26, 31, 40, 255}; //6
+    Color yellow = {255, 255, 0, 255};  //7
 
 
     return {green, red, orange, purple, cyan, blue, darkGray, yellow};
