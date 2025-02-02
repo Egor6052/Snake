e09ec98b96d0fe9ba38154df1714ae1ed2ac247e
AdminFedora <segor6052@gmail.com>
2025-02-02 23:03:52 +0200

    build for windows

---
+++ build-windows/CMakeFiles/3.30.5/CMakeSystem.cmake
@@ -0,0 +1,15 @@
+set(CMAKE_HOST_SYSTEM "Linux-6.12.8-200.fc41.x86_64")
+set(CMAKE_HOST_SYSTEM_NAME "Linux")
+set(CMAKE_HOST_SYSTEM_VERSION "6.12.8-200.fc41.x86_64")
+set(CMAKE_HOST_SYSTEM_PROCESSOR "x86_64")
+
+include("/home/admin1/Стільниця/Snake/mingw-toolchain.cmake")
+
+set(CMAKE_SYSTEM "Windows-1")
+set(CMAKE_SYSTEM_NAME "Windows")
+set(CMAKE_SYSTEM_VERSION "1")
+set(CMAKE_SYSTEM_PROCESSOR "")
+
+set(CMAKE_CROSSCOMPILING "TRUE")
+
+set(CMAKE_SYSTEM_LOADED 1)
